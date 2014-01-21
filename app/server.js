b57d5362c64de709eee1df1764c60f59627b81c1
Olli Salli <olli.salli@vincit.fi>
2014-01-21 18:58:27 +0200

    Implement GET /forecast

--- app/server.js
+++ app/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var querystring = require('querystring');
+var _ = require('underscore');
+
 var get = require('./get.js');
 
 app.get('/get/:url', function(req, res) {
@@ -12,6 +15,39 @@ app.get('/get/:url', function(req, res) {
                 .status(getRes.status)
                 .set('Content-Type', getRes.headers['content-type'])
                 .send(getRes.text);
+        }
+    });
+});
+
+app.get('/forecast', function(req, res) {
+    if (!req.query.city) {
+        res.json(400, { status: 400, message: "'city' query parameter required" });
+        return;
+    }
+
+    get('http://api.openweathermap.org/data/2.5/forecast/daily?' + querystring.stringify({
+        mode: 'json',
+        q: req.query.city,
+        units: 'metric',
+        cnt: 3
+    }), function(err, getRes) {
+        if (err || getRes.error) {
+            console.log(err || getRes.error);
+            res.json(500, { status: 500 });
+        } else {
+            res.json(200, {
+                city: req.query.city,
+                forecast: _.map(getRes.body.list, function(day) {
+                    return {
+                        weather: day.weather.description,
+                        temp: day.temp.day,
+                        wind: {
+                            speed: day.speed,
+                            dir: day.deg
+                        }
+                    };
+                })
+            });
         }
     });
 });

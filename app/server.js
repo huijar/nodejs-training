3edb98e5bd113567dd353df6f27c6904f9502ee2
Olli Salli <olli.salli@vincit.fi>
2014-01-21 20:04:10 +0200

    Move weather forecast logic to separate module

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-var querystring = require('querystring');
 var _ = require('underscore');
 
 var get = require('./get.js');
+var weather = require('./weather.js');
 
 app.get('/get/:url', function(req, res) {
     get(req.params.url, function(err, getRes) {
@@ -25,29 +25,12 @@ app.get('/forecast', function(req, res) {
         return;
     }
 
-    get('http://api.openweathermap.org/data/2.5/forecast/daily?' + querystring.stringify({
-        mode: 'json',
-        q: req.query.city,
-        units: 'metric',
-        cnt: 3
-    }), function(err, getRes) {
-        if (err || getRes.error) {
-            console.log(err || getRes.error);
+    weather.forecast(req.query.city, function(err, weather) {
+        if (err) {
+            console.log(err);
             res.json(500, { status: 500 });
         } else {
-            res.json(200, {
-                city: req.query.city,
-                forecast: _.map(getRes.body.list, function(day) {
-                    return {
-                        weather: day.weather.description,
-                        temp: day.temp.day,
-                        wind: {
-                            speed: day.speed,
-                            dir: day.deg
-                        }
-                    };
-                })
-            });
+            res.json(200, weather);
         }
     });
 });
---
+++ app/weather.js
@@ -0,0 +1,33 @@
+var querystring = require('querystring');
+var _ = require('underscore');
+
+var get = require('./get.js');
+
+module.exports = {
+    forecast: function(city, cb) {
+        get('http://api.openweathermap.org/data/2.5/forecast/daily?' + querystring.stringify({
+            mode: 'json',
+            q: city,
+            units: 'metric',
+            cnt: 3
+        }), function(err, getRes) {
+            if (err || getRes.error) {
+                cb(err || getRes.error);
+            } else {
+                cb(null, {
+                    city: city,
+                    forecast: _.map(getRes.body.list, function(day) {
+                        return {
+                            weather: day.weather.description,
+                            temp: day.temp.day,
+                            wind: {
+                                speed: day.speed,
+                                dir: day.deg
+                            }
+                        };
+                    })
+                });
+            }
+        });
+    }
+};
